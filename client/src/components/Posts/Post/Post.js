5e3027febb10a200376d3b8e2b46fe9605a97169
Saif Kamal <saif@Saifs-MacBook-Air.local>
2021-08-13 15:19:26 +0600

    Latest Commit at 3.19pm

---
+++ client/src/components/Posts/Post/Post.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import useStyles from './styles';
+
+const Post = () => {
+    // eslint-disable-next-line
+    const classes = useStyles();
+    return (
+        <h1>POST</h1>
+    );
+}
+
+export default Post;
